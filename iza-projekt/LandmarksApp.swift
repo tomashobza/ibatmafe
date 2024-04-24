51682158a8254c4958eb426b37e9cf5b909c9531
Tomáš Hobza <xhobza03@vutbr.cz>
2024-04-24 18:42:13 +0200

    feat: latest changes

--- iza-projekt/LandmarksApp.swift
+++ iza-projekt/LandmarksApp.swift
@@ -1,17 +1,33 @@
 //
-//  LandmarksApp.swift
-//  Landmarks
+//  iza_projektApp.swift
+//  iza-projekt
 //
-//  Created by Tomáš Hobza on 22.04.2024.
+//  Created by Tomáš Hobza on 23.04.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
-struct LandmarksApp: App {
+struct iza_projektApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Event.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(sharedModelContainer)
+//        .modelContainer(for: Event.self)
     }
 }
