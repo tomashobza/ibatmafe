02e51478219671ded643d4021412ab9ca74f7769
Tomáš Hobza <xhobza03@vutbr.cz>
2024-04-24 18:48:09 +0200

    Initial Commit

---
+++ iza-projekt/iza_projektApp.swift
@@ -0,0 +1,32 @@
+//
+//  iza_projektApp.swift
+//  iza-projekt
+//
+//  Created by Tomáš Hobza on 24.04.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct iza_projektApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
